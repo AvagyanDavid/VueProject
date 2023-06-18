a0ac79df71f1a5488640a6259cb88c6fdbdda541
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-06-19 02:17:41 +0300

    Подготовка и подключение данных для экрана заказов

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -8,23 +8,24 @@
   </v-container>
 
   <v-list subheader two-line flat>
-    <v-list-item>
+    <v-list-item v-for="order in orders"
+      :key="order.id">
     
       <template v-slot:prepend>
       <v-list-item-action>
       <v-checkbox
-      input-value="false"
+      input-value="order.done"
       color="primary"
       ></v-checkbox>
       </v-list-item-action>
-      
+
       </template>
-      <v-list-item-title>Title</v-list-item-title>
-      <v-list-item-subtitle>Phone</v-list-item-subtitle>
+      <v-list-item-title>{{ order.name }}</v-list-item-title>
+      <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
       <template v-slot:append>
 
       <v-list-item-action>
-      <v-btn class="primary">Open</v-btn>
+      <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
       </v-list-item-action>
       </template>
       
@@ -36,6 +37,16 @@
     data () {
       return {
       }
-    }
+    },
+    orders: [
+      {
+        id:"123",
+        name:"Kostya",
+        phone:"+7(978)000-00-05",
+        adId:"1",
+        done:true
+      }
+    ]
   }
+  
 </script>
