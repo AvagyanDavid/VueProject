dd7e8067ec3a6dc464a55aeb979654eb2c3e2a15
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-06-18 14:47:28 +0300

    Подлючение бокового меню

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn  text><v-icon icon="mdi-cake-variant"></v-icon>Link One</v-btn> 
@@ -18,5 +18,10 @@
   
 <script>
   export default {
+    data(){
+      return{
+        drawer: false,
+      }
+    }
   }
 </script>
