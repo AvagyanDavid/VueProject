8a34af765acbe898e298b7362390bdfb8aa23176
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-06-19 03:02:07 +0300

    Подключение данных к списку

--- frontend/src/views/Ads/AdListView.vue
+++ frontend/src/views/Ads/AdListView.vue
@@ -10,11 +10,11 @@
             </v-img>
           </v-col>
           <v-col xs="8">
-            <h2 class="text--primary">{{  }}</h2>
-            <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{  }}</p>
+            <h2 class="text--primary">{{ ad.title }}</h2>
+            <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
             <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" variant="text" :to="">
+            <v-btn color="primary" variant="text" :to="'/ad/'+ad.id">
             Open
             </v-btn>
             </v-card-actions>
@@ -25,11 +25,41 @@
     </v-row>
   </v-container>
 </template>
-  <script>
-    export default {
-      data () {
-        return {
-        }
+<script>
+  export default {
+    data () {
+      return { 
+        ads:[
+          {
+            title:"First",
+            desc:"First Desc",
+            promo: true,
+            src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+            id:"1"
+          },
+          {
+            title:"Second",
+            desc:"Second Desc",
+            promo: true,
+            src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+            id:"2"
+          },
+          {
+            title:"Third",
+            desc:"Thitd Desc",
+            promo: true,
+            src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+            id:"3"
+          },
+          {
+            title:"Fouth",
+            desc:"Fouth Desc",
+            promo: true,
+            src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+            id:"4"
+          }
+        ]
       }
     }
-  </script>
+  }
+</script>
