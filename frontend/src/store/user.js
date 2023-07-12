d79f3fa99ba169a7928c3f1bdc604d6b3e3be253
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-07-13 00:04:41 +0300

    Подключение лоудера на экране регистрации

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,30 +1,48 @@
 class User {
-  constructor(id, email, password) {
-  this.id = id
-  this.email = email
-  this.password = password
+  constructor(id, email, pass) {
+    this.id = id;
+    this.email = email;
+    this.pass = pass;
   }
 }
 
 export default {
-	state: {
-    user:null
+  state: {
+    user: null,
   },
-	mutations: {
-    setUser(state,payload){
-      console.log(payload)
-      state.user = payload
+  mutations: {
+    setUser(state, payload) {
+        console.log(payload);
+        state.user = payload;
     }
   },
-	actions: {
-    registerUser({commit},{email,password}){
-      // Здесь запрос на сервер для регистрации
-        commit('setUser',new User(1,email,password))
+  actions: {
+    async registerUser({commit}, {email, password}) {
+      commit('clearError');
+      commit('setLoading', true);
+        //  Запрос на сервер
+      let isRequestOk = true; // в методичке стоит false
+      let promise = new Promise(function(resolve) {
+        setTimeout(() => resolve('Done'), 3000);
+      });
+          
+      if (isRequestOk) {
+        await promise.then(() => {
+          commit('setUser', new User(1, email, password))
+          commit('setLoading', false)
+        });
+      } else {
+        await promise.then(() => {
+          commit('setLoading', false)
+          commit('setError', 'Ошибка регистрации')
+          throw 'Упс. Ошибка регистрации.'
+        })
+      }
     }
   },
-	getters: {
-    user(state){
-      return state.user
+  getters: {
+    user(state) {
+      return state.user;
     }
   }
-}
+} 
